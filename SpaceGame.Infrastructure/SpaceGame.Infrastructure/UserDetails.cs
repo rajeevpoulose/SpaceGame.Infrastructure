42a8195de3569691e0284071bcfe5623f0bdfeb3
Rajeev Paulose <rajeevpoulose@outlook.com>
2020-10-21 22:12:01 +1100

    Artifacts added

---
+++ SpaceGame.Infrastructure/SpaceGame.Infrastructure/UserDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpaceGame.Infrastructure
+{
+    public class UserDetails
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
